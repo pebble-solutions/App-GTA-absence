44e72b1383e3dfdb03c0d43d09be8005c42b6dc9
marilehoux <mari.lehoux@gmail.com>
2022-08-05 15:25:13 +0200

    proposition espace perso espace équipe

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,60 +5,96 @@
 		
 
 		<div class="row">
-			<div class="card my-3">
+			<div class="card mb-3">
+				<h3 class="card-header">Mes infos</h3>
 				<div class="card-body">
-					<h3 class="text-center list-group-item m-0">Alertes</h3>
-				</div>
-				<div class="list-group list-group-flush">
-					<a href="#" class="list-group-item list-group-item-action">
-						<div><span class="badge bg-secondary fs-6">12</span> demandes en attente validation (perso)</div>
-					</a>
-					<a href="#" class="list-group-item list-group-item-action">
-						<div><span class="badge bg-secondary fs-6">12</span> demandes  à traiter (if n+1)</div>
-					</a>
-					<a href="#" class="list-group-item list-group-item-action">
-						<div><span class="badge bg-secondary fs-6">12</span> demandes à traiter (if RH)</div>
-					</a>
+					<div class="card-group">
+						<div class="card">
+							<div class="card-header">En cours</div>
+							<div class="list-group list-group-flush">
+								<div class="d-flex justify-content-between list-group-item">
+									<span>en attente validation (if perso) </span><span class="badge bg-secondary fs-6">2</span>
+								</div>
+								<div class="d-flex justify-content-between list-group-item">
+									<span>en attente décision (if n+1) </span><span class="badge bg-secondary fs-6">6</span>
+								</div>
+								<div class="d-flex justify-content-between list-group-item">
+									<span>en attente traitement (if RH) </span><span class="badge bg-danger fs-6">24</span>
+								</div>
+							</div>
+						</div>
+						<div class="card">
+							<div class="card-header">Mon compteur</div>
+
+							<div class="list-group list-group-flush">
+								<div class="d-flex justify-content-between list-group-item">
+									<span>CP acquis </span><span class="badge bg-secondary fs-6">12</span>
+								</div>
+								<div class="d-flex justify-content-between list-group-item">
+									<span>CP pris </span><span class="badge bg-secondary fs-6">0</span>
+								</div>
+								<div class="d-flex justify-content-between list-group-item">
+									<span>SOLDE </span><span class="badge bg-secondary fs-6">12</span>
+								</div>
+							</div>
+						</div>
+					</div>
 				</div>
 			</div>
+			<div class="card mb-3">
+				<h3 class="card-header">Mon équipe</h3>
 
-			<div class="card">
-				<h3 class="list-group-item text-center">Compteurs</h3>
-
-				
-
-					
-				<div class="card-group">
-					<div class="card">
-						<p class="card-header">Matricule</p>
-						<div class="list-group list-group-flush">
-							<div class="list-group-item">congés acquis: X</div>
-							<div class="list-group-item">congés pris: Y</div>
-							<div class="list-group-item">SOLDE: Z</div>
+			<div class="card-body">
+					<div class="card-group">
+						<!-- <div class="card">
+							<p class="card-header">Matricule</p>
+							<div class="list-group list-group-flush">
+								<div class="list-group-item">congés acquis: X</div>
+								<div class="list-group-item">congés pris: Y</div>
+								<div class="list-group-item">SOLDE: </div>
+							</div>
+						</div> -->
+						<div class="card">
+							<div class="card-header">Killian</div>
+							<div class="list-group list-group-flush">
+								<div class="d-flex justify-content-between list-group-item">
+									<span>CP acquis </span><span class="badge bg-secondary fs-6">24</span>
+								</div>
+								<div class="d-flex justify-content-between list-group-item">
+									<span>CP pris </span><span class="badge bg-secondary fs-6">0</span>
+								</div>
+								<div class="d-flex justify-content-between list-group-item">
+									<span>SOLDE </span><span class="badge bg-danger fs-6">24</span>
+								</div>
+							</div>
 						</div>
-					</div>
-					<div class="card">
-						<p class="card-header">Matricule</p>
-						<div class="list-group list-group-flush">
-							<div class="list-group-item">congés acquis</div>
-							<div class="list-group-item">congés pris</div>
-							<div class="list-group-item">SOLDE</div>
+						<div class="card">
+							<div class="card-header">Guillaume</div>
+							<div class="list-group list-group-flush">
+								<div class="d-flex justify-content-between list-group-item">
+									<span>CP acquis </span><span class="badge bg-secondary fs-6">12</span>
+								</div>
+								<div class="d-flex justify-content-between list-group-item">
+									<span>CP pris </span><span class="badge bg-secondary fs-6">12</span>
+								</div>
+								<div class="d-flex justify-content-between list-group-item">
+									<span>SOLDE </span><span class="badge bg-success fs-6">0</span>
+								</div>
+							</div>
 						</div>
-					</div>
-					<div class="card">
-						<p class="card-header">Matricule</p>
-						<div class="list-group list-group-flush">
-							<div class="list-group-item">congés acquis</div>
-							<div class="list-group-item">congés pris</div>
-							<div class="list-group-item">SOLDE</div>
-						</div>
-					</div>
-					<div class="card">
-						<p class="card-header">Matricule</p>
-						<div class="list-group list-group-flush">
-							<div class="list-group-item">CP acquis</div>
-							<div class="list-group-item">CP pris</div>
-							<div class="list-group-item">SOLDE: Z</div>
+						<div class="card">
+							<div class="card-header">Sébastien</div>
+							<div class="list-group list-group-flush">
+								<div class="d-flex justify-content-between list-group-item">
+									<span>CP acquis </span><span class="badge bg-secondary fs-6">24</span>
+								</div>
+								<div class="d-flex justify-content-between list-group-item">
+									<span>CP pris </span><span class="badge bg-secondary fs-6">0</span>
+								</div>
+								<div class="d-flex justify-content-between list-group-item">
+									<span>SOLDE </span><span class="badge bg-success fs-6">Octobre</span>
+								</div>
+							</div>
 						</div>
 					</div>
 				</div>
